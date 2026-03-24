22465080731c00e6f6031f90090bd4d73e091669
rathorvijay <rathorvijay856@gmail.com>
2026-03-25 01:19:12 +0530

    add aap.us

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use('/api/', limiter);
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://newdormaz.onrender.com/',
   credentials: true,
 }));
 
